f2de44e261c91f83dbe37e61fc17e666a1a42199
kristiwert <105439205+kristiwert@users.noreply.github.com>
2022-06-18 21:38:56 +0300

    проба заполнения конфига1

--- src/main/java/com/bus/routes/busroutesapp/config/EmailConfig.java
+++ src/main/java/com/bus/routes/busroutesapp/config/EmailConfig.java
@@ -8,5 +8,36 @@ import java.util.Properties;
 
 @Configuration
 public class EmailConfig {
+    @Value("${spring.mail.host}")
+    private String mailServerHost;
 
+    @Value("${spring.mail.port}")
+    private Integer mailServerPort;
+
+    @Value("${spring.mail.username}")
+    private String mailServerUsername;
+
+    @Value("${spring.mail.password}")
+    private String mailServerPassword;
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private String mailServerAuth;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private String mailServerStartTls;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(mailServerHost);
+        mailSender.setPort(mailServerPort);
+        mailSender.setUsername(mailServerUsername);
+        mailSender.setPassword(mailServerPassword);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", mailServerAuth);
+        props.put("mail.smtp.starttls.enable", mailServerStartTls);
+        return mailSender;
+    }
 }
